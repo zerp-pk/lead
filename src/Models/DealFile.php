bdb1ca6fcf8443a387bcca34442594104ba2bbec
HafizMMoaz <hafizmoazkhalid@gmail.com>
2026-07-10 06:06:03 +0500

    Link lead & deal files to Media library (media_id)

--- src/Models/DealFile.php
+++ src/Models/DealFile.php
@@ -10,11 +10,17 @@ class DealFile extends Model
     protected $fillable = [
         'deal_id',
         'file_name',
-        'file_path'
+        'file_path',
+        'media_id',
     ];
 
     public function deal(): BelongsTo
     {
         return $this->belongsTo(Deal::class);
+    }
+
+    public function media(): BelongsTo
+    {
+        return $this->belongsTo(\Spatie\MediaLibrary\MediaCollections\Models\Media::class, 'media_id');
     }
 }
--- src/Models/LeadFile.php
+++ src/Models/LeadFile.php
@@ -10,11 +10,17 @@ class LeadFile extends Model
     protected $fillable = [
         'lead_id',
         'file_name',
-        'file_path'
+        'file_path',
+        'media_id',
     ];
 
     public function lead(): BelongsTo
     {
         return $this->belongsTo(Lead::class);
+    }
+
+    public function media(): BelongsTo
+    {
+        return $this->belongsTo(\Spatie\MediaLibrary\MediaCollections\Models\Media::class, 'media_id');
     }
 }
